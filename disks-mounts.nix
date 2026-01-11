acbfb0941afa9bd86485c3b4550cf33b76cf59a7
Sinsry <Sinsry@users.noreply.github.com>
2026-01-11 21:44:57 +0100

    Auto commit 2026-01-11_21:44

--- disks-mounts.nix
+++ disks-mounts.nix
@@ -28,8 +28,40 @@
     ];
   };
 
+  # Configuration des montages internes dans /mnt/
+  fileSystems."/mnt/Windows" = {
+    device = "/dev/disk/by-uuid/5A0820A008207D5F";
+    fsType = "ntfs";
+    options = [
+      "nofail"
+      "noperm"
+      ];
+  };
+
+  # Configuration des montages internes dans /mnt/
+  fileSystems."/mnt/DataWindozs" = {
+    device = "/dev/disk/by-uuid/363A21FE3A21BBAD";
+    fsType = "ntfs";
+    options = [
+      "nofail"
+      "noperm"
+    ];
+  };
+
+  # Configuration des montages internes dans /mnt/
+  fileSystems."/mnt/Jeux" = {
+    device = "/dev/disk/by-uuid/c5f40b61-d064-468c-932a-c3460bc762ed";
+    fsType = "ext4";
+    options = [
+      "nofail"
+      ];
+  };
+
   # Crée les points de montage
   systemd.tmpfiles.rules = [
     "d /mnt/Ventoy 0755 root root -"
-  ];
+    "d /mnt/Windows 0755 root root -"
+    "d /mnt/DataWindows 0755 root root -"
+    "d /mnt/Jeux 0755 root root -"
+    ];
 }
